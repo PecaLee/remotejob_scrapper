ec4e627de7b9d54c5fb8969307461aa8a1e6a971
Peca Lee <studiopeca.lee@gmail.com>
2020-06-07 15:11:32 +0900

    scrapper

---
+++ csv.py
@@ -0,0 +1,6 @@
+import csv
+from scrapper import job_db
+
+python_db = job_db("python")
+
+print(python_db)
--- scrapper.py
+++ scrapper.py
@@ -14,17 +14,90 @@ from bs4 import BeautifulSoup
 
 os.system("cls")
 
-keyword = input("input keyword").lower()
 
-stack_url = f"https://stackoverflow.com/jobs?r=true&q={keyword}"
-wework_url = f"https://weworkremotely.com/remote-jobs/search?term={keyword}"
-remoteok_url = f"https://remoteok.io/remote-dev+{keyword}-jobs"
-
-
-def stack_scrapper():
-    load = requests.get(stack_url)
-    soup = BeautifulSoup(load, "html.parser")
-    print(soup)
+def make_soup(url):
+    load = requests.get(url)
+    soup = BeautifulSoup(load.content, "html.parser")
+    return soup
 
 
-stack_scrapper()
+def remoteok_scrapper(keyword):
+    remoteok_url = f"https://remoteok.io/remote-dev+{keyword}-jobs"
+    db = []
+    remoteok = "https://remoteok.io"
+    soup = make_soup(remoteok_url)
+    table = soup.find("table", {"id": "jobsboard"})
+    results = table.find_all("tr", {"class": "job"})
+    for result in results:
+        job_title = result.select(
+            ".company.position.company_and_position h2")[0].text
+        job_link = result.find("a", {"class": "preventLink"}).get("href")
+        company = result.select(".companyLink h3")[0].text
+
+        jobs = {
+            "title": job_title,
+            "company": company,
+            "link": remoteok + job_link
+        }
+
+        db.append(jobs)
+
+    return db
+
+
+def stack_scrapper(keyword):
+    stack_url = f"https://stackoverflow.com/jobs?r=true&q={keyword}"
+    db = []
+    stack = "https://stackoverflow.com"
+    soup = make_soup(stack_url)
+    list_results = soup.find("div", {"class": "listResults"})
+    results = list_results.find_all("div", {"class": "fl1"})
+    for result in results:
+        job = result.find("a", {"class": "s-link"})
+        job_title = job.text
+        job_link = job.get("href")
+        company = result.select("h3 span:first-child")[0].text.strip()
+
+        jobs = {
+            "title": job_title,
+            "company": company,
+            "link": stack + job_link
+        }
+
+        db.append(jobs)
+
+    return db
+
+
+def wework_scrapper(keyword):
+    wework_url = f"https://weworkremotely.com/remote-jobs/search?term={keyword}"
+    db = []
+    wework = "https://weworkremotely.com"
+    soup = make_soup(wework_url)
+    job_container = soup.find("div", {"id": "job_list"})
+    list_results = job_container.find("section", {"id": "category-2"})
+    results = list_results.find_all("li", {"class": "feature"})
+    for result in results:
+        job_title = result.find("span", {"class": "title"}).text
+        job_link = result.find("div", {"class": "tooltip"})
+        if job_link == None:
+            job_link = result.find("a").get("href")
+        else:
+            job_link = result.select(".tooltip ~ a")[0].get("href")
+        company = result.find("span", {"class": "company"}).text
+
+        jobs = {
+            "title": job_title,
+            "company": company,
+            "link": wework + job_link
+        }
+
+        db.append(jobs)
+
+    return db
+
+
+def job_db(keyword):
+    db = remoteok_scrapper(keyword) + \
+        stack_scrapper(keyword) + wework_scrapper(keyword)
+    return db
